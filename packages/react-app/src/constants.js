6474ea0e695bedea2d2aabd831e5e2172c1a3281
mercuricchloride <mercuryprotocol@protonmail.com>
2022-01-10 08:27:01 -0700

    constants

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,5 +1,6 @@
 // MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
 <<<<<<< HEAD
+<<<<<<< HEAD
 export const INFURA_ID = "7b0e75d38d424750b92791477924d133";
 
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
@@ -10,15 +11,313 @@ export const INFURA_ID = "460f40a260564ac4a4f4b3fffb032dad";
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
 export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
 >>>>>>> 0b840342 (Initial commit)
+=======
+export const INFURA_ID = "d0bf96c03a064b3eaaeab7f77983d43c";
+
+// MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
+export const ETHERSCAN_KEY = "KDHMCWMT1RQRRNQC89J8HCEWUYEUYDBYBF";
+>>>>>>> 461ff54a (constants)
 
 // BLOCKNATIVE ID FOR Notify.js:
 export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 export const ALCHEMY_KEY = "oKxs-03sij-U_N0iOlrSsZFr29-IqbuF";
 
 =======
 >>>>>>> 0b840342 (Initial commit)
+=======
+// EXTERNAL CONTRACTS
+
+export const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+
+export const DAI_ABI = [
+  {
+    inputs: [{ internalType: "uint256", name: "chainId_", type: "uint256" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "src", type: "address" },
+      { indexed: true, internalType: "address", name: "guy", type: "address" },
+      { indexed: false, internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: true,
+    inputs: [
+      { indexed: true, internalType: "bytes4", name: "sig", type: "bytes4" },
+      { indexed: true, internalType: "address", name: "usr", type: "address" },
+      { indexed: true, internalType: "bytes32", name: "arg1", type: "bytes32" },
+      { indexed: true, internalType: "bytes32", name: "arg2", type: "bytes32" },
+      { indexed: false, internalType: "bytes", name: "data", type: "bytes" },
+    ],
+    name: "LogNote",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, internalType: "address", name: "src", type: "address" },
+      { indexed: true, internalType: "address", name: "dst", type: "address" },
+      { indexed: false, internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "DOMAIN_SEPARATOR",
+    outputs: [{ internalType: "bytes32", name: "", type: "bytes32" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "PERMIT_TYPEHASH",
+    outputs: [{ internalType: "bytes32", name: "", type: "bytes32" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "address", name: "", type: "address" },
+    ],
+    name: "allowance",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "usr", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "approve",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "usr", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "burn",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "decimals",
+    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [{ internalType: "address", name: "guy", type: "address" }],
+    name: "deny",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "usr", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "mint",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "src", type: "address" },
+      { internalType: "address", name: "dst", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "move",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "name",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "nonces",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "holder", type: "address" },
+      { internalType: "address", name: "spender", type: "address" },
+      { internalType: "uint256", name: "nonce", type: "uint256" },
+      { internalType: "uint256", name: "expiry", type: "uint256" },
+      { internalType: "bool", name: "allowed", type: "bool" },
+      { internalType: "uint8", name: "v", type: "uint8" },
+      { internalType: "bytes32", name: "r", type: "bytes32" },
+      { internalType: "bytes32", name: "s", type: "bytes32" },
+    ],
+    name: "permit",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "usr", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "pull",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "usr", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "push",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [{ internalType: "address", name: "guy", type: "address" }],
+    name: "rely",
+    outputs: [],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "symbol",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "dst", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "transfer",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: false,
+    inputs: [
+      { internalType: "address", name: "src", type: "address" },
+      { internalType: "address", name: "dst", type: "address" },
+      { internalType: "uint256", name: "wad", type: "uint256" },
+    ],
+    name: "transferFrom",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    payable: false,
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: "version",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    constant: true,
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "wards",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    payable: false,
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+export const NETWORK = chainId => {
+  for (const n in NETWORKS) {
+    if (NETWORKS[n].chainId === chainId) {
+      return NETWORKS[n];
+    }
+  }
+};
+
+>>>>>>> 461ff54a (constants)
 export const NETWORKS = {
   localhost: {
     name: "localhost",
@@ -26,20 +325,28 @@ export const NETWORKS = {
     chainId: 31337,
     blockExplorer: "",
 <<<<<<< HEAD
+<<<<<<< HEAD
     rpcUrl: "http://" + (global.window ? window.location.hostname : "localhost") + ":8545",
 =======
     rpcUrl: "http://" + window.location.hostname + ":8545",
 >>>>>>> 0b840342 (Initial commit)
+=======
+    rpcUrl: "http://" + window.location.hostname + ":8545",
+>>>>>>> 461ff54a (constants)
   },
   mainnet: {
     name: "mainnet",
     color: "#ff8b9e",
     chainId: 1,
 <<<<<<< HEAD
+<<<<<<< HEAD
     rpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
 =======
     rpcUrl: `https://mainnet.infura.io/v3/${INFURA_ID}`,
 >>>>>>> 0b840342 (Initial commit)
+=======
+    rpcUrl: `https://mainnet.infura.io/v3/${INFURA_ID}`,
+>>>>>>> 461ff54a (constants)
     blockExplorer: "https://etherscan.io/",
   },
   kovan: {
@@ -85,16 +392,22 @@ export const NETWORKS = {
     blockExplorer: "https://blockscout.com/poa/xdai/",
   },
 <<<<<<< HEAD
+<<<<<<< HEAD
   polygon: {
     name: "polygon",
 =======
   matic: {
     name: "matic",
 >>>>>>> 0b840342 (Initial commit)
+=======
+  matic: {
+    name: "matic",
+>>>>>>> 461ff54a (constants)
     color: "#2bbdf7",
     chainId: 137,
     price: 1,
     gasPrice: 1000000000,
+<<<<<<< HEAD
 <<<<<<< HEAD
     rpcUrl: "https://polygon-rpc.com/",
     blockExplorer: "https://polygonscan.com/",
@@ -103,6 +416,11 @@ export const NETWORKS = {
     faucet: "https://faucet.matic.network/",
     blockExplorer: "https://explorer-mainnet.maticvigil.com//",
 >>>>>>> 0b840342 (Initial commit)
+=======
+    rpcUrl: "https://rpc-mainnet.maticvigil.com",
+    faucet: "https://faucet.matic.network/",
+    blockExplorer: "https://explorer-mainnet.maticvigil.com//",
+>>>>>>> 461ff54a (constants)
   },
   mumbai: {
     name: "mumbai",
@@ -111,6 +429,7 @@ export const NETWORKS = {
     price: 1,
     gasPrice: 1000000000,
     rpcUrl: "https://rpc-mumbai.maticvigil.com",
+<<<<<<< HEAD
 <<<<<<< HEAD
     faucet: "https://faucet.polygon.technology/",
     blockExplorer: "https://mumbai.polygonscan.com/",
@@ -265,4 +584,9 @@ export const NETWORK = chainId => {
       return NETWORKS[n];
     }
   }
+=======
+    faucet: "https://faucet.matic.network/",
+    blockExplorer: "https://mumbai-explorer.matic.today/",
+  },
+>>>>>>> 461ff54a (constants)
 };
